fc136016f484fe1cdb16a77afa6cc3d942ee2882
Саят Бектенбай <sayat.b@a2data.ai>
2022-09-20 16:08:30 +0600

    Update

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+
+import { AuthContext } from '../store/auth.context';
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState('');
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  useEffect(() => {
+    axios.get(
+      `https://react-native-course-7c95c-default-rtdb.firebaseio.com/message.json?auth=${token}`
+    ).then(response => {
+      setMessage(response.data);
+    });
+  }, []);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
@@ -16,11 +32,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 32,
+    padding: 32
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 8,
-  },
+    marginBottom: 8
+  }
 });
